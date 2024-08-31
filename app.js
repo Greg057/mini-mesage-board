a95e0902baf38775b99f8c2a5c2b989e4ca93a16
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-08-31 14:36:23 +0200

    add form and post request to add message

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
 const express = require("express")
 const app = express()
 
+app.use(express.urlencoded({ extended: true }));
+
+const indexRouter = require("./routes/indexRouter")
+
+app.use("/", indexRouter);
+
 app.set("views", "./views")
 app.set("view engine", "ejs")
 
-const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-];
-
-
-app.get("/", (req, res) => {
-    res.render("index")
-})
-
-app.get("/message", (req, res) => {
-    res.render("message")
-})
 
 app.listen(8000)
